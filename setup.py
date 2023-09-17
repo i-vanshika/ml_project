b0062c0b3cb180fd0de587da59c105f4445d9f70
Vanshika Verma <vanshika.j.verma@gmail.com>
2023-09-17 13:19:38 +0530

    setup

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-from setuptools import find_packages,setup 
+from setuptools import find_packages,setup
 from typing import List
-# This is basically metadata information for the entire project
+
 
 HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return the list of requirements
+    This function will return the lsit of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n", "") for req in requirements]
+        requirements=[req.replace("\n", "")for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+    return requirements        
 
 setup(
-    name='ml_project',
+    name='mlproject',
     version='0.0.1',
     author='Vanshika',
     author_email='vanshika.j.verma@gmail.com',
